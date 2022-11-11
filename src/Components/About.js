e70407c8366666fcdda6e406c5ec2ed0a06232ec
Dana <danafeldman97@gmail.com>
2022-11-11 12:56:53 -0500

    v1 html for about page done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,47 @@
 const About = () => {
   return (
-    <div className="about">
-      <h2>About Us</h2>
-    </div>
+    <>
+      <section className="about">
+        <h2>About Refrigeration Versification</h2>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fuga velit cum! Placeat delectus autem amet qui est? Repudiandae repellendus temporibus eos nisi aliquam vel, repellat nostrum voluptatum et consectetur quis, delectus inventore? Cumque quasi dicta eum aliquid libero aut explicabo, laudantium blanditiis, ex reprehenderit odit necessitatibus error. Accusantium atque distinctio et nulla veritatis labore, ducimus, dolore quae, recusandae quod architecto blanditiis obcaecati vero eos excepturi optio eaque. Aut tenetur incidunt sint unde? Pariatur consectetur voluptatum itaque dolorum, odio molestiae cumque perferendis repellat quibusdam rem sed omnis eos reiciendis earum quam nihil quaerat. Laboriosam odio inventore mollitia dolor quaerat quidem?</p>
+
+        <h2>About the Developers</h2>
+        <div>
+          <h3>Charlotte</h3>
+          <p>lil bio</p>
+          <a href="https://github.com/chaarz" target="_blank" rel="noopener noreferrer">github </a>
+          <a href="https://www.linkedin.com/in/charlotteduppre/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+          <a href="https://charlotteduppre.com/index.html#" target="_blank" rel="noopener noreferrer">portfolio</a>
+        </div>
+
+        <div>
+          <h3>Dana</h3>
+          <p>lil bio</p>
+          <a href="https://github.com/DanaFCodes" target="_blank" rel="noopener noreferrer">github </a>
+          <a href="https://www.linkedin.com/in/dana-filatov/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+          <a href="https://danafilatov.com/" target="_blank" rel="noopener noreferrer">portfolio</a>
+        </div>
+
+        <div>
+          <h3>Jen</h3>
+          <p>lil bio</p>
+          <a href="https://github.com/luu-jennifer" target="_blank" rel="noopener noreferrer">github </a>
+          <a href="https://www.linkedin.com/in/jencodes/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+          {/* website link */}
+        </div>
+
+        <div>
+          <h3>Tina</h3>
+          <p>lil bio</p>
+          <a href="https://github.com/tinacllu" target="_blank" rel="noopener noreferrer">github </a>
+          <a href="https://www.linkedin.com/in/tinacllu/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+          <a href="https://www.tinalu.ca/" target="_blank" rel="noopener noreferrer">portfolio</a>
+        </div>
+
+      </section>
+
+    </>
   );
 };
 
