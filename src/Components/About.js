a42739e9b0bdf5478672d8e6c23558bc5e7c24f0
Dana <danafeldman97@gmail.com>
2022-11-13 13:24:03 -0500

    added missing closing div tag lol

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,118 +11,121 @@ const About = () => {
 
         <h2>About the Developers</h2>
 
-        <div className= "topRow">
-        <div className="profileMagnet char">
-          <div className="profileTop">
-          <h3>Charlotte</h3>
-            {/* <img src={} alt="headshot profile picture" /> */}
+        <div className="topRow">
+          <div className="profileMagnet char">
+            <div className="profileTop">
+              <h3>Charlotte</h3>
+              {/* <img src={} alt="headshot profile picture" /> */}
+            </div>
+            <p>lil bio</p>
+            <a href="https://github.com/chaarz" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/charlotteduppre/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://charlotteduppre.com/index.html#" target="_blank" rel="noopener noreferrer">
+              <i class="fa-solid fa-address-card"></i>
+            </a>
           </div>
-          <p>lil bio</p>
-          <a href="https://github.com/chaarz" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/charlotteduppre/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="https://charlotteduppre.com/index.html#" target="_blank" rel="noopener noreferrer">
-            <i class="fa-solid fa-address-card"></i>
-          </a>
-        </div>
 
-        <div className="profileMagnet dana">
-          <div className="profileTop">
-          <h3>Dana Filatov</h3>
-            {/* <img src={} alt="headshot profile picture" /> */}
+          <div className="profileMagnet dana">
+            <div className="profileTop">
+              <h3>Dana Filatov</h3>
+              {/* <img src={} alt="headshot profile picture" /> */}
             </div>
             
             <p>Hey! My name is Dana (she/her), and I'm an artist turned Frontend Web Developer navigating my way from an ocean of paint and clay to a sea of zeros and ones.
 
               Trained in visual communicaton and creative problem-solving, I'm shifting my analogue skills to the screen🪄✨</p>
-          <a href="https://github.com/DanaFCodes" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/dana-filatov/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="https://danafilatov.com/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-solid fa-address-card"></i>
-          </a>
-        </div>
+            <a href="https://github.com/DanaFCodes" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/dana-filatov/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://danafilatov.com/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-solid fa-address-card"></i>
+            </a>
+          </div>
         </div>
 
         <div className="bottomRow">
-        <div className="profileMagnet jen">
-          <div className="profileTop">
-          <h3>Jen</h3>
-            {/* <img src={} alt="headshot profile picture" /> */}
+          <div className="profileMagnet jen">
+            <div className="profileTop">
+              <h3>Jen</h3>
+              {/* <img src={} alt="headshot profile picture" /> */}
+            </div>
+            <p>lil bio</p>
+            <a href="https://github.com/luu-jennifer" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/jencodes/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://jencodes.com/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-solid fa-address-card"></i>
+            </a>
           </div>
-          <p>lil bio</p>
-          <a href="https://github.com/luu-jennifer" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/jencodes/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="https://jencodes.com/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-solid fa-address-card"></i>
-          </a>
-        </div>
+          </div>
 
-        <div className="profileMagnet tina">
-          <div className="profileTop">
-          <h3>Tina</h3>
-            {/* <img src={} alt="headshot profile picture" /> */}
-          </div>
+          <div className="profileMagnet tina">
+            <div className="profileTop">
+              <h3>Tina</h3>
+              {/* <img src={} alt="headshot profile picture" /> */}
+            </div>
             <p>Hello! My name is Tina and I am a scientist turned front-end developer! I love to solve problems and create aesthetic applications with clean and re-useable code. I would like to use my skills to develop engaging applications tailored to each client's needs.
               Outside of coding, you can find me trying new recipes and attempting to keep my plants alive. Feel free to reach out if you are in need of a developer, in the mood for cool science facts, or just for fun!</p>
-          <a href="https://github.com/tinacllu" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-github"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/tinacllu/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="https://www.tinalu.ca/" target="_blank" rel="noopener noreferrer">
-            <i class="fa-solid fa-address-card"></i>
-          </a>
+            <a href="https://github.com/tinacllu" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/tinacllu/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://www.tinalu.ca/" target="_blank" rel="noopener noreferrer">
+              <i class="fa-solid fa-address-card"></i>
+            </a>
           </div>
 
-        <div>
-          <h3>Charlotte</h3>
-          <p>lil bio</p>
-          <a href="https://github.com/chaarz" target="_blank" rel="noopener noreferrer">github </a>
-          <a href="https://www.linkedin.com/in/charlotteduppre/" target="_blank" rel="noopener noreferrer"> linkedin </a>
-          <a href="https://charlotteduppre.com/index.html#" target="_blank" rel="noopener noreferrer">portfolio</a>
-        </div>
+          <div>
+            <h3>Charlotte</h3>
+            <p>lil bio</p>
+            <a href="https://github.com/chaarz" target="_blank" rel="noopener noreferrer">github </a>
+            <a href="https://www.linkedin.com/in/charlotteduppre/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+            <a href="https://charlotteduppre.com/index.html#" target="_blank" rel="noopener noreferrer">portfolio</a>
+          </div>
 
-        <div>
-          <h3>Dana</h3>
-          <p>lil bio</p>
-          <a href="https://github.com/DanaFCodes" target="_blank" rel="noopener noreferrer">github </a>
-          <a href="https://www.linkedin.com/in/dana-filatov/" target="_blank" rel="noopener noreferrer"> linkedin </a>
-          <a href="https://danafilatov.com/" target="_blank" rel="noopener noreferrer">portfolio</a>
-        </div>
+          <div>
+            <h3>Dana</h3>
+            <p>lil bio</p>
+            <a href="https://github.com/DanaFCodes" target="_blank" rel="noopener noreferrer">github </a>
+            <a href="https://www.linkedin.com/in/dana-filatov/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+            <a href="https://danafilatov.com/" target="_blank" rel="noopener noreferrer">portfolio</a>
+          </div>
 
-        <div>
-          <h3>Jen</h3>
-          <p>lil bio</p>
-          <a href="https://github.com/luu-jennifer" target="_blank" rel="noopener noreferrer">github </a>
-          <a href="https://www.linkedin.com/in/jencodes/" target="_blank" rel="noopener noreferrer"> linkedin </a>
-          {/* website link */}
-        </div>
+          <div>
+            <h3>Jen</h3>
+            <p>lil bio</p>
+            <a href="https://github.com/luu-jennifer" target="_blank" rel="noopener noreferrer">github </a>
+            <a href="https://www.linkedin.com/in/jencodes/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+            {/* website link */}
+          </div>
 
-        <div>
-          <h3>Tina</h3>
-          <p>lil bio</p>
-          <a href="https://github.com/tinacllu" target="_blank" rel="noopener noreferrer">github </a>
-          <a href="https://www.linkedin.com/in/tinacllu/" target="_blank" rel="noopener noreferrer"> linkedin </a>
-          <a href="https://www.tinalu.ca/" target="_blank" rel="noopener noreferrer">portfolio</a>
+          <div>
+            <h3>Tina</h3>
+            <p>lil bio</p>
+            <a href="https://github.com/tinacllu" target="_blank" rel="noopener noreferrer">github </a>
+            <a href="https://www.linkedin.com/in/tinacllu/" target="_blank" rel="noopener noreferrer"> linkedin </a>
+            <a href="https://www.tinalu.ca/" target="_blank" rel="noopener noreferrer">portfolio</a>
 
-        </div>
+          </div>
 
       </section>
 
     </>
   );
+
 };
+
 
 export default About;
