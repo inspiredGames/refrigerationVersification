41156c258f3bda444ebf44896d63469c68c098a9
tinacllu <tina.lu0107@gmail.com>
2022-11-14 17:11:51 -0500

    dependency array for netlify

--- src/Components/WordBank.js
+++ src/Components/WordBank.js
@@ -23,7 +23,7 @@ const WordBank = ({ words, searchQuery }) => {
             setShowWords(true);
             setShowHelperWords(true);
         }
-    }, [])
+    }, [width])
 
     useEffect(() => {
         const newWordBankArray = [];
