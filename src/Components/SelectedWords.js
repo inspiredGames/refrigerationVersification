3e01840fc8f3d3eaef24abb518451ebff6b947f5
tinacllu <tina.lu0107@gmail.com>
2022-11-15 22:27:55 -0500

    it works! but flexbox

--- src/Components/SelectedWords.js
+++ src/Components/SelectedWords.js
@@ -2,60 +2,96 @@ import { useState, useLayoutEffect, useRef } from 'react';
 import { useDrag } from '@use-gesture/react';
 import { useEffect } from 'react';
 
-
-const SelectedWords = ({item, index, handleRemoveWord, fridgeHeight, fridgeWidth, fridgeX, fridgeY, userSelection}) => {
-    // console.log(`selectedword has rerendered ${item.word}`)
+const SelectedWords = ({item, index, handleRemoveWord, fridgeHeight, fridgeWidth, fridgeX, fridgeY, fridgeRef, userSelection}) => {
     const [poemWord, setPoemWord] = useState({ x: 0, y: 0 });
-    const [marginX, setMarginX] = useState(0);
-    const [marginY, setMarginY] = useState(0);
-    const [wordWidth, setWordWidth] = useState(0);
-    const [wordHeight, setWordHeight] = useState(0);
 
     const [wordX, setWordX] = useState(0);
     const [wordY, setWordY] = useState(0);
     const wordRef = useRef(null);
 
+    // const [arrayLength, setArrayLength] = useState(userSelection.length);
 
-    // set word position to absolute once it has rendered on the page(?)
-    // reset style for positioning on initial render
-    // update wordX, wordY, marginX, marginY everytime userselection updates in fridge
+    // useEffect(() => {
+    //     // const wordX = wordRef.current.getBoundingClientRect().left;
+    //     // const wordY = wordRef.current.getBoundingClientRect().top;
 
 
+    //     // setWordWidth(wordRef.current.getBoundingClientRect().width);
+    //     // setWordHeight(wordRef.current.getBoundingClientRect().height);
 
-    // set position, top, left to empty strings on initial layout
-    // everytime user selection changes, set top and left to last dragged position 
-    useLayoutEffect(() => {
-        const wordX = wordRef.current.getBoundingClientRect().left;
-        const wordY = wordRef.current.getBoundingClientRect().top;
+    //     // setMarginX(wordX-fridgeX);
+    //     // setMarginY(wordY-fridgeY);
+        
+    //     if (wordX !== 0 && wordY !== 0) {
+    //         wordRef.current.position = 'absolute';
+        
+    //         wordRef.current.style.top = '';
+    //         wordRef.current.style.left = '';
+    //     } else {
+    //         console.log(wordX, wordY, 'wordX and y')
+    //     }
+        
 
-        setWordWidth(wordRef.current.getBoundingClientRect().width);
-        setWordHeight(wordRef.current.getBoundingClientRect().height);
 
-        setMarginX(wordX-fridgeX);
-        setMarginY(wordY-fridgeY);
-        console.log(wordX, wordY, `wordX and wordY ${item.word}`)
-        console.log(wordX-fridgeX, 'marginx')
-        console.log(wordY-fridgeY, 'marginy')
+    // }, [userSelection])
 
-        console.log(fridgeHeight, fridgeWidth, 'fridge height and width', fridgeX, fridgeY, 'fridge X and Y');
-        wordRef.current.position = '';
-        // wordItem.style.position = '';
-        wordRef.current.style.top = '';
-        wordRef.current.style.left = '';
-        // console.log(wordItem.style.position, wordItem.style.top, wordItem.style.left)
-
-        console.log(wordRef.current.getBoundingClientRect())
-    }, [])
 
     useEffect(() => {
-        // wordRef.current.style.top = poemWord.y;
-        // wordRef.current.style.left = poemWord.x;
-        wordRef.current.position = 'absolute';
 
-        // setPoemWord({x:wordRef.current.getBoundingClientRect().left , y:wordRef.current.getBoundingClientRect().top})
+        if (wordX !== 0 || wordY !== 0) {
+            wordRef.current.position = 'absolute';
+            wordRef.current.style.top = wordY;
+            wordRef.current.style.left = wordX;
+            wordRef.current.style.display = 'inline';
+            console.log('yayyyyy')
+
+        }
+        
 
     }, [userSelection])
 
+    useEffect(() => {
+        setWordX(wordRef.current.getBoundingClientRect().left);
+        setWordY(wordRef.current.getBoundingClientRect().top);
+        console.log(wordRef.current)
+        console.log(wordX, wordY, 'from getwordcoords')
+    }, [poemWord])
+
+
+    // useEffect(() => {
+        
+    //     wordRef.current.position = 'absolute';
+
+    //     // const wordX = wordRef.current.getBoundingClientRect().left;
+    //     // const wordY = wordRef.current.getBoundingClientRect().top;
+
+    //     // setMarginX(wordX-fridgeX);
+    //     // setMarginY(wordY-fridgeY);
+        
+    //     // console.group(`'from useeffect' ${item.word}`)
+    //     // console.log(wordX, wordY, 'wordX and wordY')
+    //     // console.log(wordX-fridgeX, wordY-fridgeY, 'marginx and y')
+    //     // console.log(fridgeX, fridgeY, 'fridge x and y')
+    //     // console.groupEnd();
+
+        
+    // }, [userSelection])
+
+
+    // const bind = useDrag((params) => {
+    //     setPoemWord({
+    //         x: params.offset[0],
+    //         y: params.offset[1],
+    //     });
+    // }, {
+    //     bounds: {
+    //         left: (0-marginX),
+    //         right: (fridgeWidth-marginX-wordWidth),
+    //         top: (0-marginY),
+    //         bottom: (fridgeHeight-marginY-wordHeight),
+    //     },
+        
+    // });
 
     const bind = useDrag((params) => {
         setPoemWord({
@@ -63,26 +99,19 @@ const SelectedWords = ({item, index, handleRemoveWord, fridgeHeight, fridgeWidth
             y: params.offset[1],
         });
     }, {
-        bounds: {
-            left: (0-marginX),
-            right: (fridgeWidth-marginX-wordWidth),
-            top: (0-marginY),
-            bottom: (fridgeHeight-marginY-wordHeight),
-        },
+        bounds: fridgeRef,
     });
-
+    
     return (
         <li 
-            // onClick = {(e) => {handleClick(e)}}
             ref={wordRef}
             {...bind()} 
             style={{ 
-                // position: 'absolute',
                 top: poemWord.y,
                 left: poemWord.x,
                 touchAction: 'none',
-                
             }}
+
             id={item.word.replace(/\s/g, '')}
             onDoubleClick={(e) => {handleRemoveWord(e.target.textContent)}}>
                 {item.word}
